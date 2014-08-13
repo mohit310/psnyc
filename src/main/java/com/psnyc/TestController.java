e05131261b3b68beead314cfa478337b11b33448
Mohit Keswani <mohit.keswani@gmail.com>
2014-08-13 02:39:02 -0400

    added spring

---
+++ src/main/java/com/psnyc/TestController.java
@@ -0,0 +1,24 @@
+package com.psnyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by mk on 8/13/2014.
+ */
+@Controller
+@EnableAutoConfiguration
+public class TestController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
+    }
+}
